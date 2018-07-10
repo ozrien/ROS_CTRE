f14bcf25b758428e94ea04c805ed9040c4dc4857
ryan <hobo.ryang@gmail.com>
2018-07-09 20:07:24 -0400

    Readme updates

--- ctreROS_ws/launch_base_control.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-source ~/Documents/CTRE/ROS_CTRE/ctreROS_ws/devel/setup.bash
-
-sudo ~/Documents/ubuntu_setup/linux_settings/bash_scripts/canableStart.sh
-roslaunch ros_control_boilerplate diff_drive_test.launch interface:=can0 &
-sleep 10
-roslaunch ros_control_boilerplate teleop_twist_joy.launch
--- ctreROS_ws/src/ros_control_boilerplate/scripts/lidar_chmod.sh
+++ ctreROS_ws/src/ros_control_boilerplate/scripts/lidar_chmod.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-
-echo "HIIIII"
 sudo chmod a+rw /dev/ttyUSB0
